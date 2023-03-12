7a763be34766b4b98f84f42837e7d6f74cca1317
Calvert <wongcalvert@gmail.com>
2023-03-11 23:18:56 -0800

    Learn terraform but the code is incomplete and wont work

---
+++ .github/aws/variables.tf
@@ -0,0 +1,6 @@
+variable "AWS_IAM_ACCESS_KEY" {}
+variable "AWS_IAM_SECRET_KEY" {}
+variable "REGION" {}
+# variable "SSH_PUB_KEY_PATH" {}
+variable "SSL_CERTIFICATE_ARN" {}
+variable "ROUTE53_HOSTED_ZONE_NAME" {}
